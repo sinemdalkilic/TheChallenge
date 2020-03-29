13bfa8c5276db115d532969362c7da87024fe8ca
sinemdalkilic <sinemdalkilic@gmail.com>
2020-03-29 21:47:15 +0300

    Initial Commit

---
+++ TheChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheChallenge
+//
+//  Created by Sinem on 29/03/20.
+//  Copyright © 2020 TheChallenge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
